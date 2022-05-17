8da43c46c78910abfca528bbda7fb2db304a3e19
diva-olvya <94207287+diva-olvya@users.noreply.github.com>
2022-05-17 13:37:22 +0700

    Initial commit

---
+++ application/views/admin/dataPegawai.php
@@ -0,0 +1,54 @@
+<div class="container-fluid" style="margin-bottom: 100px;">
+
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800"><?= $title ?></h1>
+
+    </div>
+
+    <?= $this->session->flashdata('pesan'); ?>
+    <a class="mb-2 mt-2 btn btn-sm btn-success" href="<?= base_url('admin/dataPegawai/tambahData') ?>"><i class="fas fa-plus"></i> Tambah Pegawai</a>
+
+    <table class="table table-bordered table-striped mt-2">
+        <tr>
+            <th class="text-center">No</th>
+            <th class="text-center">NIK</th>
+            <th class="text-center">Pegawai</th>
+            <th class="text-center">Jns.Kelamin</th>
+            <th class="text-center">Jabatan</th>
+            <th class="text-center">Tgl.Masuk</th>
+            <th class="text-center">Status</th>
+            <th class="text-center">Photo</th>
+            <th class="text-center">Hak Akses</th>
+            <th class="text-center">Action</th>
+        </tr>
+
+        <?php $no = 1;
+        foreach ($pegawai as $p) : ?>
+            <tr>
+                <td class="text-center"><?= $no++ ?></td>
+                <td class="text-center"><?= $p->nik ?></td>
+                <td class="text-center"><?= $p->nama_pegawai ?></td>
+                <td class="text-center"><?= $p->jenis_kelamin ?></td>
+                <td class="text-center"><?= $p->jabatan ?></td>
+                <td class="text-center"><?= $p->tanggal_masuk ?></td>
+                <td class="text-center"><?= $p->status ?></td>
+                <td class="text-center"><img src="<?= base_url() . 'assets/photo/' . $p->photo ?>" width="75px"></td>
+
+                <?php if ($p->hak_akses == '1') { ?>
+                    <td>Admin</td>
+                <?php } else { ?>
+                    <td>Pegawai</td>
+                <?php } ?>
+
+                <td class="text-center">
+                    <center>
+                        <a class="btn btn-sm btn-primary" href="<?= base_url('admin/dataPegawai/updateData/' . $p->id_pegawai) ?>"><i class="fas fa-edit"></i></a>
+                        <a onclick="return confirm('Yakin Hapus?')" class="btn btn-sm btn-danger" href="<?= base_url('admin/dataPegawai/deleteData/' . $p->id_pegawai) ?>"><i class="fas fa-trash"></i></a>
+                    </center>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+
+
+</div>
---
+++ application/views/pegawai/formGantiPassword.php
@@ -0,0 +1,32 @@
+<div class="container-fluid">
+
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800"><?= $title ?></h1>
+
+    </div>
+
+
+    <div class="card" style="width: 50%;">
+        <div class="card-body">
+            <form method="post" action="<?= base_url('pegawai/gantiPassword/gantiPasswordAksi') ?>">
+
+                <div class="form-froup">
+                    <label> Password Baru</label>
+                    <input type="password" name="passBaru" class="form-control">
+                    <?= form_error('passBaru', '<div class="text-small text-danger"></div>') ?>
+                </div>
+
+                <div class="form-froup">
+                    <label>Ulangi Password</label>
+                    <input type="password" name="ulangPass" class="form-control">
+                    <?= form_error('ulangPass', '<div class="text-small text-danger"></div>') ?>
+                </div>
+                <br>
+
+                <button type="submit" class="btn btn-success">Simpan</button>
+
+            </form>
+        </div>
+    </div>
+
+</div>
